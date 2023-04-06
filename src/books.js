4885cbcd4e9325fe44254a3dbabb451a257c4f95
Daniel Condon <123604423+conddj06@users.noreply.github.com>
2023-04-05 23:04:36 -0400

    Add files via upload

---
+++ src/books.js
@@ -0,0 +1,33 @@
+function findAuthorById(authors, id) {
+  let found = authors.find((author) => author.id === id);
+  return found;
+}
+//find
+function findBookById(books, id) {
+  let foundBooks = books.find((book) => book.id === id);
+  return foundBooks; 
+}
+//filter
+function partitionBooksByBorrowedStatus(books) {
+ let booksReturned = books.filter((book) => book.borrows.every((borrow) => borrow.returned === true)
+ );
+ let booksBorrowed = books.filter((book) => book.borrows.some((borrow) => borrow.returned === false)
+ );
+ let finalArray = [[...booksBorrowed], [...booksReturned]];
+ return finalArray;
+}
+// find & map
+function getBorrowersForBook(book, accounts) {
+  return book.borrows.map((borrow) => {
+    let account = accounts.find((account) => account.id === borrow.id);
+    return { ...borrow, ...account };
+  })
+  .slice(0, 10);
+}
+
+module.exports = {
+  findAuthorById,
+  findBookById,
+  partitionBooksByBorrowedStatus,
+  getBorrowersForBook,
+};
